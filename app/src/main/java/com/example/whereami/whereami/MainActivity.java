fa390f3dbe88e1a65423263946cf96835d785f47
Hevelend <nutella-ferrero@hotmail.fr>
2014-12-14 13:17:11 -0500

    Import support.v4

--- app/src/main/java/com/example/whereami/whereami/MainActivity.java
+++ app/src/main/java/com/example/whereami/whereami/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
 public class MainActivity extends Activity {
 
     @Override
--- opencv2/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ opencv2/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: F:\\AndroidStudio\\WhereAmI\\opencv2\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Quentin-ROG\\AndroidStudioProjects\\WhereAmIGit\\WhereAmI\\opencv2\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
